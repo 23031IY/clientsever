d29e3f665ddf5b2f4769e1e8b0eea521053fc6fc
ishii yuuki <bp23031@shibaura-it.ac.jp>
2026-01-14 16:20:08 +0900

    リザルトを修正

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,8 +14,8 @@ import javax.swing.SwingUtilities;
 public class Main {
 
     // サーバ設定 (ポートやパスは環境に合わせてください)
-    private static final String CLIENT_SERVER_URI = "ws://localhost:8080/ChinchiroServer/ws";
-    private static final String APP_SERVER_URI    = "ws://localhost:8082/GameServer/game";
+    private static final String CLIENT_SERVER_URI = "ws://10.17.147.5:8080/ChinchiroServer/ws";
+    private static final String APP_SERVER_URI    = "ws://10.17.147.5:8082/GameServer/game";
 
     public static void main(String[] args) {
         // SwingのUI処理は専用のスレッド(EDT)で行うのが定石です
